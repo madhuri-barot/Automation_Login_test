019f4e789a702fa749134a7e19e1c2ff919bcdb0
madhuri-barot <madhuri@prioxis.com>
2025-05-14 18:16:22 +0530

    Update LoginTest.java

--- src/test/java/com/test/LoginTest.java
+++ src/test/java/com/test/LoginTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.Assert;
 
 public class LoginTest {
 
